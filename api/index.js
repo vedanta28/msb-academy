73a70f142b50d2666da81bfd32b5a044f850bba3
Vedanta Mohapatra <vedantamohapatra@Vedantas-MacBook-Air.local>
2023-03-28 08:46:18 +0530

    Update index.js and Install Node Modules

--- api/index.js
+++ api/index.js
@@ -7,3 +7,23 @@ const PORT = process.env.PORT || 6900;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// let PORT = process.env.PORT;
+
+// function startServer() {
+//   const server = app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//   });
+
+//   server.on('error', (err) => {
+//     if (err.code === 'EADDRINUSE') {
+//       // Port is already in use, choose another available port
+//       PORT++;
+//       startServer();
+//     } else {
+//       console.error(err);
+//     }
+//   });
+// }
+
+// startServer();

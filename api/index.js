3d4a110ea988df09ee9f19dcfe709a7e6142917e
Koustav Sen <senkoustav9@gmail.com>
2023-03-28 09:31:18 +0530

    Added Security Modules

--- api/index.js
+++ api/index.js
@@ -1,14 +1,46 @@
+// third-party modules
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 
-// Keep this the last requirement
+// Load environment variables from .env file: Keep this the first requirement
+require('dotenv').config(); 
+
+// Uncaught Exceptions For Synchronous Code
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception. Shutting Down");
+  console.log(err.name, err.message, err.stack);
+  process.exit(1);
+});
+
 // Connecting to Database
 require("./utils/connectDatabase").connect();
 
+// CODE BEGINS HERE
 const app = express();
-const PORT = process.env.PORT || 6900;
 
+// Middlewares
+// Sets security HTTP headers
+app.use(helmet());
+// Limit requests from same IP to our api
+app.use(
+  "/api",
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP",
+  })
+);
+// Middleware to parse body into req.body
+app.use(express.json({ limit: "10kb" }));
+// Data sanitisation against NoSQL query injection -> "email" : { "$gt" : ""}, we need to prevent this
+app.use(mongoSanitize());
+// Data sanitisation against XSS attacks
+app.use(xss());
+
+const PORT = process.env.PORT || 6900;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
@@ -17,7 +49,6 @@ app.listen(PORT, () => {
 //   const server = app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 //   });
-
 //   server.on('error', (err) => {
 //     if (err.code === 'EADDRINUSE') {
 //       // Port is already in use, choose another available port
@@ -28,5 +59,13 @@ app.listen(PORT, () => {
 //     }
 //   });
 // }
-
 // startServer();
+
+// Unhandled Rejection for Failed Promises
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection. Shutting Down");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});

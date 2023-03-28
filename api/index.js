5d09f31aef3f6894c4f0079970301f75990fc344
Koustav Sen <senkoustav9@gmail.com>
2023-03-28 09:16:09 +0530

    Added Utility File for Database Connection

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load env variables
+require('dotenv').config(); // Load environment variables from .env file
+
+// Keep this the last requirement
+// Connecting to Database
+require("./utils/connectDatabase").connect();
+
 const app = express();
 const PORT = process.env.PORT || 6900;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// let PORT = process.env.PORT;
 
 // function startServer() {
 //   const server = app.listen(PORT, () => {

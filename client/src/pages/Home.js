e659e753ddf7640eb5e125c9066f4997c59c29e3
Koustav Sen <senkoustav9@gmail.com>
2023-03-29 08:00:13 +0530

    Fixed Responsiveness of Navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,7 @@
-// import Navbar from "../components/NavBar";
 const Home = () => {
     return(
         <div className="Home">
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
-            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi facilis nobis iusto animi odit. </h1>
+           
         </div>
     )
 }

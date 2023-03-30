f8c4b4a9d54d243e59cf6074aae3a7fc066883ee
Vedanta Mohapatra <vedantamohapatra@Vedantas-MacBook-Air.local>
2023-03-30 11:24:27 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,31 @@ import Button from "@mui/material/Button";
 
 const Home = () => {
   return (
-    <div className="Base">
+    <div className="Base" style={{height:"1000vh"}}>
+      <div className="Home">
+        <div className="HomeText">
+          <h1
+            style={{
+              fontFamily: "Kanit, sans-serif",
+              fontSize: "48px",
+            }}
+          >
+            Crack your goal with India's top educators
+          </h1>
+          <h5
+            style={{
+              fontFamily: "Kanit, sans-serif",
+              fontSize: "16px",
+              margin: "5px",
+            }}
+          >
+            Over <span style={{ color: "#0ABC81" }}>8,00,000+</span> learners
+            trust us for their preparation
+          </h5>
+          <Button variant="contained">Start Learning</Button>
+        </div>
+        <div className="HomeImage" />
+      </div>
       <div className="Home">
         <div className="HomeText">
           <h1

e397a39ef38f053cb38b3fb80fd74bbac8080781
Koustav Sen <senkoustav9@gmail.com>
2023-03-30 08:19:34 +0530

    Fixed Some Merge Issues and Integrated Home Page With Profile

--- client/src/pages/CheckOut.js
+++ client/src/pages/CheckOut.js
@@ -1,56 +1,61 @@
-import Head from 'next/head';
-import { CardMedia, Button, Box, Container, Stack, Typography, Card, CardContent, CardActions, Unstable_Grid2 as Grid } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import PaymentsIcon from '@mui/icons-material/Payments';
-// import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { AccountProfile } from '../sections/account/account-profile';
-import { AccountProfileDetails } from '../sections/account/account-profile-details';
+import {
+  CardMedia,
+  Button,
+  Box,
+  Container,
+  Stack,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import PaymentsIcon from "@mui/icons-material/Payments";
 
-
-const Data = [{
-  Picture: "./koustav.png",
-  Name: "Koustav Sen",
-  Price: 100,
-  Description: "This Course will teach you life"
-},
-{
-  Picture: "./koustav.png",
-  Name: "Cute Girl ",
-  Price: 100,
-  Description: "This Course will give you wife"
-},
-{
-  Picture: "./koustav.png",
-  Name: "Koustav Sen",
-  Price: 400,
-  Description: "This Course will take your life"
-},
-{
-  Picture: "./koustav.png",
-  Name: "Koustav Sen",
-  Price: 100,
-  Description: "This Course will teach you life"
-}
+const Data = [
+  {
+    Picture: "./koustav.png",
+    Name: "Koustav Sen",
+    Price: 100,
+    Description: "This Course will teach you life",
+  },
+  {
+    Picture: "./koustav.png",
+    Name: "Cute Girl ",
+    Price: 100,
+    Description: "This Course will give you wife",
+  },
+  {
+    Picture: "./koustav.png",
+    Name: "Koustav Sen",
+    Price: 400,
+    Description: "This Course will take your life",
+  },
+  {
+    Picture: "./koustav.png",
+    Name: "Koustav Sen",
+    Price: 100,
+    Description: "This Course will teach you life",
+  },
 ];
 
-function TotalCard(Data){
-  const Total=Data.reduce((acc,curr)=>acc+curr.Price,0);
-  return(
+function TotalCard(Data) {
+  const Total = Data.reduce((acc, curr) => acc + curr.Price, 0);
+  return (
     <Card sx={{ maxWidth: 600 }}>
-
       <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-
-
-        <Typography variant="body2" >
-          Your Total is ₹{Total}
-        </Typography>
-
+        <Typography variant="body2">Your Total is ₹{Total}</Typography>
       </CardContent>
 
       <CardActions>
-      <Button  variant="Text" startIcon={<PaymentsIcon />} style={{ verticalAlign: "middle",color:"green" }}>
+        <Button
+          variant="Text"
+          startIcon={<PaymentsIcon />}
+          style={{ verticalAlign: "middle", color: "green" }}
+        >
           Proceed to Pay
-          </Button>
+        </Button>
       </CardActions>
     </Card>
   );
@@ -58,37 +63,44 @@ function TotalCard(Data){
 function MediaCard(Data) {
   return (
     <Card sx={{ minWidth: 600 }}>
-
       <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-
         <Container style={{ display: "flex", padding: "10px" }}>
           <CardMedia
             component="img"
-
             image={Data.Picture}
             alt={Data.Name}
-            style={{ height:"100px", width:"200px",marginRight: "20px", verticalAlign: "middle" }}
+            style={{
+              height: "100px",
+              width: "200px",
+              marginRight: "20px",
+              verticalAlign: "middle",
+            }}
           />
-          <Typography component="div" variant='button' noWrap='true' style={{ verticalAlign: "middle" }}>
+          <Typography
+            component="div"
+            variant="button"
+            noWrap="true"
+            style={{ verticalAlign: "middle" }}
+          >
             {Data.Name}
           </Typography>
-          
-
-
         </Container>
 
-        <Typography variant="body2" >
-          {Data.Description}
-        </Typography>
-
+        <Typography variant="body2">{Data.Description}</Typography>
       </CardContent>
 
       <CardActions>
-      <Button  startIcon={<DeleteIcon />} style={{ verticalAlign: "middle",color:"red" }}>
+        <Button
+          startIcon={<DeleteIcon />}
+          style={{ verticalAlign: "middle", color: "red" }}
+        >
           Remove from Cart
-          </Button>
-        <Typography variant="body2" style={{ verticalAlign: "middle",marginRight:"50"}}>
-          ₹{Data.Price}  
+        </Button>
+        <Typography
+          variant="body2"
+          style={{ verticalAlign: "middle", marginRight: "50" }}
+        >
+          ₹{Data.Price}
         </Typography>
       </CardActions>
     </Card>
@@ -96,38 +108,25 @@ function MediaCard(Data) {
 }
 
 const Page = () => (
-  <div className='Base'>
-    <Head>
-      <title>
-        Checkout
-      </title>
-    </Head>
+  <div className="Base">
+    <title>Checkout</title>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth="lg">
         <Stack spacing={3}>
-          <Card>
-          {Data.map(MediaCard)}
-          
-          </Card>
+          <Card>{Data.map(MediaCard)}</Card>
           {TotalCard(Data)}
         </Stack>
-
       </Container>
     </Box>
   </div>
 );
 
-
-Page.getLayout = (page) => (
-  <div>
-    {page}
-  </div>
-);
+Page.getLayout = (page) => <div>{page}</div>;
 
 export default Page;

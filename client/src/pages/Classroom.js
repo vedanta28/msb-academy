b354905df6caea4f5305acb0c67a7e19ee093f64
Vedanta Mohapatra <vedantamohapatra@Vedantas-MacBook-Air.local>
2023-03-31 08:48:48 +0530

    Update Classroom.js

--- client/src/pages/Classroom.js
+++ client/src/pages/Classroom.js
@@ -1,9 +1,65 @@
-import React from "react";
+import CourseCardSimple from "../components/CourseCardSimple";
+const data = [
+  {
+    Picture: "./koustav.png",
+    Language: "English",
+    InstuctorName: "Koustav Sen",
+    CourseName: "Complete Course on Computer Networks - Part I",
+    Description:
+      "In this course, Koustav will cover Computer Networks. All the important topics will be discussed in detail and would be helpful for aspirants preparing for the GATE exam.",
+    TotalVideos: "10",
+    StartDate: "21 Apr, 2021",
+    EndDate: "7 May, 2021",
+    TotalVideoLengh: "2h 30m",
+    Price: "₹500"
+  },
+  {
+    Picture: "./koustav.png",
+    Language: "English",
+    InstuctorName: "Koustav Sen",
+    CourseName: "Complete Course on Computer Networks - Part II",
+    Description:
+      "In this course, Koustav will cover Computer Networks. All the important topics will be discussed in detail and would be helpful for aspirants preparing for the GATE exam.",
+    TotalVideos: "10",
+    StartDate: "21 Apr, 2021",
+    EndDate: "7 May, 2021",
+    TotalVideoLengh: "2h 30m",
+    Price: "₹500"
+  },
+  {
+    Picture: "./koustav.png",
+    Language: "English",
+    InstuctorName: "Koustav Sen",
+    CourseName: "Complete Course on Computer Networks - Part III",
+    Description:
+      "In this course, Koustav will cover Computer Networks. All the important topics will be discussed in detail and would be helpful for aspirants preparing for the GATE exam.",
+    TotalVideos: "10",
+    StartDate: "21 Apr, 2021",
+    EndDate: "7 May, 2021",
+    TotalVideoLengh: "2h 30m",
+    Price: "₹500"
+  },
+  {
+    Picture: "./koustav.png",
+    Language: "English",
+    InstuctorName: "Koustav Sen",
+    CourseName: "Complete Course on Computer Networks - Part IV",
+    Description:
+      "In this course, Koustav will cover Computer Networks. All the important topics will be discussed in detail and would be helpful for aspirants preparing for the GATE exam.",
+    TotalVideos: "10",
+    StartDate: "21 Apr, 2021",
+    EndDate: "7 May, 2021",
+    TotalVideoLengh: "2h 30m",
+    Price: "₹500"
+  },
+];
 
-export default function Classroom() {
-  return (
-    <div className="Base">
-      <h1>My Courses: Coming Soon</h1>
-    </div>
-  );
-}
+const Page = () => (
+  <div className="Courses">
+    <div style={{ margin: "auto" }}>{data.map(CourseCardSimple)}</div>
+  </div>
+);
+
+Page.getLayout = (page) => <div>{page}</div>;
+
+export default Page;

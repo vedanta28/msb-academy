083c479ec07b41a5d2dc4629583106bba00fcff9
vedanta28 <20cs02001@iitbbs.ac.in>
2023-04-01 20:33:13 +0530

    Added Models

---
+++ api/models/userModel.js
@@ -0,0 +1,73 @@
+const moongose = require("mongoose");
+const userSchema = new moongose.Schema(
+  {
+    fname: {
+      type: String,
+      required: [true, "A User must have a name"],
+    },
+    lname: {
+      type: String,
+    },
+    state: {
+      type: String,
+      required: [true, "A User must have a state"]
+    },
+    country: {
+      type: String,
+      required: [true, "A User must have a country"]
+    },
+    password: {
+      type: String,
+      required: [true, "A User must have a password"],
+    },
+    image: {
+      type: String,
+      default: "default.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Instructor"],
+      default: "Student",
+    },
+    phoneNo: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /\d{10}/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number. Must be 10 digits long.`,
+      },
+      required: [true, "Phone number is required"],
+    },
+    emailId: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
+    dob: {
+      type: Date,
+      required: [true, "Date of Birth is required"],
+    },
+    courseTaken: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    wishlist: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { collection: "Users" }
+);
+const User = moongose.model("User", userSchema);
+module.exports = User;

6450ca6eefa487924c15d7869a9b4759b4f43663
vedanta28 <20cs02001@iitbbs.ac.in>
2023-11-19 08:52:21 +0530

    Remove Hardcoded Valued for env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3lSYXQmHqLC7tofKwVVZv5lWETW3JAR4",
-  authDomain: "msb-academy-5e348.firebaseapp.com",
-  projectId: "msb-academy-5e348",
-  storageBucket: "msb-academy-5e348.appspot.com",
-  messagingSenderId: "87812721423",
-  appId: "1:87812721423:web:74d41d6251f47db7bc8c7a",
-  measurementId: "G-NFG81613VJ"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
